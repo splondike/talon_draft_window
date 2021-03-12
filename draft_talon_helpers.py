dff3de8ce69d79259129faf62083cc843fb8f615
Stefan Schneider-Kennedy <code@stefansk.name>
2021-03-12 22:17:24 +1100

    Add TextArea undo/redo workaround

--- draft_talon_helpers.py
+++ draft_talon_helpers.py
@@ -93,6 +93,122 @@ class EditActions:
         return ""
 
 
+from talon import cron
+class UndoWorkaround:
+    """
+    Workaround for the experimental textarea's undo being character by character.
+    This keeps a debounced undo history. Can be deleted once this todo item is
+    fixed: https://github.com/talonvoice/talon/issues/254#issuecomment-789149734
+    """
+
+    # Set this to False if you want to turn it off, or just delete all references
+    # to this class
+    enable_workaround = True
+
+    # Stack of (text_value, selection) tuples representing the undo stack
+    undo_stack = []
+    # Stack of (text_value, selection) tuples representing the redo stack
+    redo_stack = []
+    # Used by the timer to check when the text has stopped changing
+    pending_undo = None
+
+    # timer handle
+    timer_handle = None
+
+    @classmethod
+    def start_logger(cls, reset_undo_stack: bool):
+        if reset_undo_stack:
+            cls.undo_stack = []
+            cls.redo_stack = []
+
+        cls.stop_logger()
+        cls.timer_handle = cron.interval("500ms", cls._log_changes)
+
+    @classmethod
+    def stop_logger(cls):
+        if cls.timer_handle is not None:
+            cron.cancel(cls.timer_handle)
+        cls.timer_handle = None
+        cls.pending_undo = None
+
+    @classmethod
+    def perform_undo(cls):
+        if len(cls.undo_stack) == 0:
+            return
+
+        curr_text = draft_manager.area.value
+        curr_sel = draft_manager.area.sel
+        text, sel = cls.undo_stack[-1]
+        if text == curr_text:
+            cls.undo_stack.pop()
+            if len(cls.undo_stack) == 0:
+                return
+
+            # Most of the time (unless user has only just finished updating) the
+            # top of the stack will have the same contents as the text area. In
+            # this case pop again to get a bit lower. We should never have the
+            # same text twice, hence we don't need a loop.
+            text, sel = cls.undo_stack[-1]
+
+        # Remember the current state in the redo stack
+        cls.redo_stack.append((curr_text, curr_sel))
+        draft_manager.area.value = text
+        draft_manager.area.sel = sel
+
+        cls.pending_undo = (text, sel)
+
+    @classmethod
+    def perform_redo(cls):
+        if len(cls.redo_stack) == 0:
+            return
+
+        curr_text = draft_manager.area.value
+        curr_sel = draft_manager.area.sel
+        text, sel = cls.redo_stack.pop()
+
+        # Remember the current state in the undo stack if we haven't already
+        if len(cls.undo_stack) == 0 or cls.undo_stack[-1][0] != curr_text:
+            cls.undo_stack.append((curr_text, curr_sel))
+
+        draft_manager.area.value = text
+        draft_manager.area.sel = sel
+
+        cls.pending_undo = (text, sel)
+
+    @classmethod
+    def _log_changes(cls):
+        """
+        If the text and cursor position hasn't changed for two interval iterations
+        (1s) and the undo stack doesn't match the current state, then add to the stack.
+        """
+
+        curr_val = draft_manager.area.value
+        # Turn the Span into a tuple, because we can't == Spans
+        curr_sel = (draft_manager.area.sel.left, draft_manager.area.sel.right)
+        curr_state = (curr_val, curr_sel)
+
+        state_stack_mismatch = (
+            len(cls.undo_stack) == 0 or
+            # Only want to update the undo stack if the value has changed, not just
+            # the selection
+            curr_state[0] != cls.undo_stack[-1][0]
+        )
+
+        if cls.pending_undo == curr_state and state_stack_mismatch:
+            cls.undo_stack.append(curr_state)
+            # Clear out the redo stack because we've changed the text
+            cls.redo_stack = []
+        elif cls.pending_undo != curr_state:
+            cls.pending_undo = curr_state
+        else:
+            # The text area text is not changing, do nothing
+            pass
+
+
+if UndoWorkaround.enable_workaround:
+    ctx_focused.action("edit.undo")(UndoWorkaround.perform_undo)
+    ctx_focused.action("edit.redo")(UndoWorkaround.perform_redo)
+
 @mod.action_class
 class Actions:
     def draft_show(text: Optional[str] = None):
@@ -101,6 +217,7 @@ class Actions:
         """
 
         draft_manager.show(text)
+        UndoWorkaround.start_logger(text is not None)
         ctx.tags = ["user.draft_window_showing"]
 
     def draft_hide():
@@ -109,6 +226,7 @@ class Actions:
         """
 
         draft_manager.hide()
+        UndoWorkaround.stop_logger()
         ctx.tags = []
 
     def draft_select(
